8b016d758fdd236e605519ce619c3c5d72793599
Beasty002 <xasbin12@gmail.com>
2023-10-31 17:08:29 +0545

    buttons added

--- admin_area/index.php
+++ admin_area/index.php
@@ -10,20 +10,20 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 
 <body>
     <!--navbar  -->
-    <div class="container-fluid p-0"">
+    <div class="container-fluid p-0" ">
                                     <!-- first bar -->
-        <nav class=" navbar navbar-expand-lg navbar-light bg-info">
-        <div class=" container-fluid">
-            <img src="../images/logo.png" class="logo" alt="">
-            <nav class=" navbar navbar-expand-lg">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="" class="nav-link">Welcome guest</a>
+        <nav class=" navbar navbar-expand-lg navbar-light bg-info ">
+        <div class=" container-fluid ">
+            <img src=" ../images/logo.png " class=" logo " alt=" ">
+            <nav class=" navbar navbar-expand-lg ">
+                <ul class=" navbar-nav ">
+                    <li class=" nav-item ">
+                        <a href=" " class=" nav-link ">Welcome guest</a>
                     </li>
                 </ul>
             </nav>
@@ -31,16 +31,28 @@
         </nav>
 
         <!-- second barr -->
-        <div class="bg-light">
-            <h3 class="text-center p-3">Manage Details</h3>
+        <div class=" bg-light ">
+            <h3 class=" text-center p-3 ">Manage Details</h3>
         </div>
 
         <!-- 3rd bar -->
-        <div class="row">
-            <div class="col-md-12 bg-secondary p-1">
-                <div>
-                    <a href="#"><img src="../images/mango.jpg" class="admin_img" alt=""></a>
-                    <p class="text-light text-center">Admin Name</p>
+        <div class=" row ">
+            <div class=" col-md-12 bg-secondary p-1 d-flex align-items-center ">
+                <div class=" px-5 ">
+                    <a href=" # "><img src=" ../images/orange.jpg " alt=" " class=" admin_img "></a>
+                    <p class=" text-light text-center ">Admin name</p>
+                </div>
+                <div class=" button text-center ">
+                    <button class=" my-3 "><a href=" " class=" nav-link text-light bg-info my-1 ">Insert products</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">View Products</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">Insert Categories</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">View Categories</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">Insert Brands</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">View Brands</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">All orders</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">All payments</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">List Users</a></button>
+                    <button><a href=" " class=" nav-link text-light bg-info my-1 ">Logout</a></button>
                 </div>
             </div>
         </div>
@@ -52,12 +64,16 @@
 
 
 
+<!-- foooooooooooooooooootereeeeeeeeeeeeeeeeeeee -->
+    <div class=" bg-info p-3 text-center mt-4">
+        <p>All rights reserverd by beasty @2020</p>
+    </div>
+    </div>
 
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js "
+        integrity=" sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL " crossorigin=" anonymous ">
     </script>
 </body>
 
